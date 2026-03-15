7eb2b5aec9298027f266b1dfdcb8441e18287307
nufayer <nufayer6543@gmail.com>
2026-03-15 15:48:18 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>
+                In the Countries
+            </h1>
+        </div>
+    );
+};
+
+export default Countries;
