79c1eec14592716e63d137d0dc7961f200d36d46
nufayer <nufayer6543@gmail.com>
2026-03-16 17:52:08 +0600

    added select buttons and css styles'

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css';
 
 const Country = ( {country} ) => {
+    const [visited, setVisited] = useState(false);
 
     const handleVisited = () =>{
-        console.log('button clicked')
+        if(visited){
+            setVisited(false)
+        }
+        else{
+            setVisited(true)
+        }
     }
     
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}` }>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt}></img>
             <h3>Name: {country.name.common} </h3>
             <p>Population: {country.population.population}</p>
             <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
